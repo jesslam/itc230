489a86a78d2a93a2b1c696d05655a4f8d6900f46
Jessica Lam <jess@chaplin.local>
2019-07-18 15:43:05 -0700

    HW 3 - Express Yourself

--- index.js
+++ index.js
@@ -1,35 +1,91 @@
-// 'use strict'
-//const express = require("express");
-//const bodyParser = require("body-parser");
-//const app = express();
-const qs = require("querystring");
-const http = require("http")
+"use strict"
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 const data = require("./lib/album.js");
-const fs = require("fs"); 
-/* app.set('port', process.env.PORT || 3000);
-app.use(express.static(__dirname + '/public')); //set location for static files
-app.use(bodyParser.urlencoded({extended: true})); //parse form submissions */
+/* const qs = require("querystring");
+const http = require("http")
+const fs = require("fs");  */
 
-/* //log req methods
-app.use(function(req, res){
-    console.log(`${req.method}, request for '${req.url}' - ${JSON.stringify(req.body)}`);
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + "./public")); //set location for static files
+app.use(bodyParser.urlencoded({extended: true})); //parse form submissions
+
+const handlebars = require("express-handlebars");
+app.engine(".html", handlebars({extname: '.html', defaultLayout: false}));
+app.set("view engine", ".html"); 
+
+/* //send static file as response
+app.get('/', (req, res) => {
+    res.type('text/html');
+    res.sendFile(__dirname + '/public/home.html');
 }); */
 
-/* const handlebars = require("express-handlebars");
-app.engine(".html", handlebars({extname: '.html', defaultLayout: false}));
-app.set("view engine", ".html"); */
+//send content of 'home' view
+app.get('/', (req, res) => {
+    res.render('home', {library: data.getAll()});
+});
 
-//http server listen request response
-//const http = require("http");
+//send plain text response
+app.get('/about', (req, res) => {
+    res.type('text/plain');
+    res.send('About page');
+});
+
+app.get('/delete', (req, res) => {
+    let result = data.deleteItem(req.query.album); //delete book object
+    let liblength = data.getAll().length;
+    res.render("delete", {
+        album: req.query.album,
+        result: result,
+        length: liblength,
+        query: req.body.keyword
+
+    });
+    console.log(req.query.album); //display parsed querystring object
+});
+
+app.get('/detail', (req, res) => {
+    let found = data.getItem(req.query.album); //get album object
+    res.render("detail", {
+        artist: req.query.artist,
+        song: req.query.song,
+        result: found
+    });
+});
+
+//handle form submit response
+app.post('/detail', (req, res) => {
+    console.log(req.body);
+    // res.type('text/html');
+    let found = data.getItem(req.body.keyword);
+    res.render('detail', {
+        artist: req.body.artist,
+        song: req.body.song,
+        result: found,
+        album: data.getAll(),
+        query: req.body.keyword
+    });
+});
+
+//define 404 handler
+app.use( (req, res) => {
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not found');
+});
+
+app.listen(app.get('port'), () => {
+    console.log('Express started at ' + __dirname);
+});
+
+/* //http server listen request response
 http.createServer((req, res) => {
     let url = req.url.split("?"); //separate url from querystring
     let query = qs.parse(url[1]); //convert querystring to object
     let path = url[0].toLowerCase();
-
-
-    console.log(query);
-
-    switch(path) {
+ */
+/*     switch(path) {
         case '/':
             // const fs = require("fs");
             fs.readFile("./public/home.html", (err, data) => {
@@ -60,14 +116,15 @@ http.createServer((req, res) => {
             res.writeHead(200, {'Content-Type': 'text/plain'});
             let results = (found) ? JSON.stringify(found) : "Not Found";
             res.end('Results for ' + query.album + '\n' + results);
-
             break;
 
         case '/delete':
             let find = data.deleteItem(query.album); //find album by index
             res.writeHead(200, {'Content-Type': 'text/plain'});
             let del = (find) ? JSON.stringify(find) : "Not Deleted";
-            res.end(query.album + " has been " + del) 
+            res.end(query.album + " has been " + del);
+            
+            break; 
 
         default:
             res.writeHead(404, {'Content-Type': 'text/plain'});
@@ -75,56 +132,5 @@ http.createServer((req, res) => {
             break;
     }
 }).listen(process.env.PORT || 3000);
-
-/* //send static file as response
-app.get('/', (req, res) => {
-    console.log(req.query)
-    res.type('text/html');
-    res.sendFile(__dirname + '/public/home.html');
-}); */
-
-/* //send content of 'home' view
-app.get('/', (req, res) => {
-    res.render('home');
-});
  */
-/* //send plain text response
-app.get('/about', (req, res) => {
-    res.type('text/plain');
-    res.send('About ' + req.query.name + '\'s page');
-}); */
 
-/* //handle form submit response
-app.post('/detail', (req, res) => {
-    console.log(req.body.keyword);
-    res.type('text/plain');
-    res.render('detail', req.body.keyword);
-    let findAlbum = album.getAlbum(req.body);
-    return findAlbum;
-    ;
-
-});
-
-//define 404 handler
-app.use( (req, res) => {
-    res.type('text/plain');
-    res.status(404);
-    res.send('404 - Not found');
-});
-
-app.listen(app.get('port'), () => {
-    console.log('Express started at ' + __dirname);
-});
-
-app.get('/getItem', (req, res) => {
-    res.type('text/plain');
-    res.render("/lib/album")
-    console.log(req.query); //display parsed querystring object
-})
-
-app.post('/getAll', (req, res) => {
-    res.json(libary);
-    console.log(req.body); //display parsed form submission
-});
-
- */
