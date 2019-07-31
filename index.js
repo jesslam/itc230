a46f502fa31a6a1065632c86fa8adae02b7422de
Jessica Lam <jess@chaplin.hsd1.wa.comcast.net>
2019-07-30 23:26:59 -0700

    MongoDB data storage - add, delete

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const data = require("./lib/album.js");
+const Albums = require("./models/album.js");
 /* const qs = require("querystring");
 const http = require("http")
 const fs = require("fs");  */
+
+
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + "./public")); //set location for static files
@@ -23,7 +26,11 @@ app.get('/', (req, res) => {
 
 //send content of 'home' view
 app.get('/', (req, res) => {
-    res.render('home', {library: data.getAll()});
+    Albums.find({}, (err, items) => {
+        if (err) return next(err);
+        //console.log(items.length);
+        res.render('home', {library: items });
+    });
 });
 
 //send plain text response
@@ -33,51 +40,58 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/delete', (req, res) => {
-    let result = data.deleteItem(req.query.album); //delete album object
-    let liblength = data.getAll().length;
-    res.render("delete", {
-        album: req.query.album,
-        result: result,
-        length: liblength,
-        query: req.body.keyword
-
+        Albums.deleteOne({album: req.query.album}, (err, result) => {
+            if (err) return next(err);
+            //console.log(result);
+            Albums.countDocuments({},(err, count) => {
+                res.type('text/html');
+                res.render("delete", {
+                    album: req.query.album, 
+                    count: count,
+                    deleted: (result.deletedCount>0) 
+            });
+        });
     });
-    console.log(req.query.album); //display parsed querystring object
 });
 
 app.get('/detail', (req, res) => {
-    let found = data.getItem(req.query.album); //get album object
-    res.render("detail", {
-        artist: req.query.artist,
-        song: req.query.song,
-        result: found
-    });
-});
+    Albums.findOne({'album' : req.query.album}, (err, item) => {
+        if (err) return next(err);
+        console.log(item);
+        res.type('text/html');
+        res.render("detail", {
+            result: item,
+            query: req.query.album
+        });
+      }); 
+}); 
 
 //handle form submit response
 app.post('/detail', (req, res) => {
-    //console.log(req.body);
-    // res.type('text/html');
-    let found = data.getItem(req.body.keyword);
-    res.render("detail", {
-        artist: req.body.artist,
-        song: req.body.song,
-        result: found,
-        album: data.getAll(),
-        query: req.body.keyword
+    Albums.findOne({'album' : req.query.keyword}, (err, item) => {
+        if (err) return next(err);
+        console.log(item);
+        res.type('text/html');
+        res.render("detail", {
+            result: item,
+            query: req.query.keyword
+        });
     });
 });
 
 //add to data module
-app.get('/add', (req, res) => {
-    let result = data.addItem(req.query);
-    console.log(req.query)
-    let length = data.getAll().length
-    console.log(result)
-    console.log('Result is ' + length);
-    res.render("detail", {
-        result: result,
-        query: req.query
+app.post('/add', (req, res) => {
+    let newEntry = {'artist': req.body.artist, 'song': req.body.song, 'album': req.body.album};
+    Albums.updateOne({'album': req.body.album}, newEntry, {upsert:true}, (err) => {
+        //console.log(result);
+        if (err) return (err);
+        res.type('text/html');
+        Albums.countDocuments({},(err, count)=> {
+            res.render('add', {
+                result: req.body,
+                count: count
+            });
+        });
     });
 });
 
@@ -91,59 +105,3 @@ app.use( (req, res) => {
 app.listen(app.get('port'), () => {
     console.log('Express started at ' + __dirname);
 });
-
-/* //http server listen request response
-http.createServer((req, res) => {
-    let url = req.url.split("?"); //separate url from querystring
-    let query = qs.parse(url[1]); //convert querystring to object
-    let path = url[0].toLowerCase();
- */
-/*     switch(path) {
-        case '/':
-            // const fs = require("fs");
-            fs.readFile("./public/home.html", (err, data) => {
-                if (err) return console.error(err);
-                res.writeHead(200, {'Content-Type' : 'text/html'});
-                res.end(data.toString());
-            });
-            break;
-
-        case '/about':
-            fs.readFile("./public/about.html", (err, data) => {
-                if (err) return console.error(err),
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.end(data.toString());
-            });
-            break;
-
-        case '/detailall':
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            let all = data.getAll();
-            let allresults = (all) ? JSON.stringify(all) : "Not Found";
-            res.write('All results are:' + "\n" + allresults);
-            res.end("");     
-            break;
-
-        case '/detail':
-            let found = data.getItem(query.album); //get album object
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            let results = (found) ? JSON.stringify(found) : "Not Found";
-            res.end('Results for ' + query.album + '\n' + results);
-            break;
-
-        case '/delete':
-            let find = data.deleteItem(query.album); //find album by index
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            let del = (find) ? JSON.stringify(find) : "Not Deleted";
-            res.end(query.album + " has been " + del);
-            
-            break; 
-
-        default:
-            res.writeHead(404, {'Content-Type': 'text/plain'});
-            res.end('404 - Page not found')
-            break;
-    }
-}).listen(process.env.PORT || 3000);
- */
-
